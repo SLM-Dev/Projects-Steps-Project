e7c3be54f650ae2497660a2958bce0569c94578b
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2021-02-16 20:59:16 +0000

    creating javascript section variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+// using querty selector all it brings them all in as a node list  which is similar to an array
+const circles = document.querySelectorAll('.circle')

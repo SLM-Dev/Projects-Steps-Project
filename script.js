3ebbc9c21a488a793d9ce89580e261690384bafe
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2021-02-17 02:46:37 +0000

    added event for prev next created update function

--- script.js
+++ script.js
@@ -3,3 +3,39 @@ const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 // using querty selector all it brings them all in as a node list  which is similar to an array
 const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive =circles.length
+    }
+        // console.log(currentActive)
+    update()
+})
+
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+    
+        // console.log(currentActive)
+    update()
+
+})
+
+
+function update() {
+    circles.forEach((cirlce, idx )  => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+    } else {
+        cirlce.classList.remove('active')
+    }
+    })
+}
